8a9210ea3ac04aeac99f36257fd2805cd340f4a1
masonwright <masonwright14@gmail.com>
2013-08-03 17:49:55 -0500

    bug fixes and extensions to garch analysis tools

--- src/python/garch.py
+++ src/python/garch.py
@@ -1,5 +1,8 @@
 from scipy.optimize import minimize
+from scipy import stats
+from numpy import mean, var
 from math import log, exp, sqrt
+from random import randint, uniform
 import stockData
 import dateHandler
 import optionAnalysis
@@ -62,6 +65,7 @@ def garchFutureDaily(omega, alpha, beta, sList, tradingDays):
 # sigma(i)^2 = omega + alpha * u(i-1)^2 + beta * sigma(i-1)^2
 def garchParams(sList):
     # initial guess taken from Hull, chapter 22 on volatility estimation with GARCH(1,1)
+    # myGuess = [0.000001347, 0.08339, 0.9101]
     myGuess = [0.000001347, 0.08339, 0.9101]
 
     # 0 < omega, alpha, beta. alpha < 1, beta < 1
@@ -85,6 +89,72 @@ def garchParams(sList):
     if listLikelihood(result, sList) < listLikelihood(myGuess, sList):
         return myGuess
     return result
+
+# like garchParams, but uses my own stochastic hill-climbing search with restarts to find
+# parameters.
+def myNaiveGarchParams(sList):
+    # initial guess taken from Hull, chapter 22 on volatility estimation with GARCH(1,1)
+    initialGuess = [0.000001347, 0.08339, 0.9101]
+    initialLikelihood = listLikelihood(initialGuess, sList)
+
+    bestGuess = initialGuess
+    bestLikelihood = initialLikelihood
+
+    # how many times to restart from the initial guess
+    restarts = 30
+    
+    # how many steps to take from the start. each step changes just one parameter of the three,
+    # in the direction in which likelihood increases most (or none, if both directions lead to decreases)
+    stepsPerRun = 50
+    while restarts > 0:
+        # restart from the initial guess
+        currentGuess = initialGuess
+        currentLikelihood = initialLikelihood
+        # reset number of steps to take for this restart
+        stepsLeft = stepsPerRun
+        while stepsLeft > 0:
+            # pick a random parameter to change: 0 for omega, 1 for alpha, 2 for beta
+            direction = randint(0,2)
+            # pick a random multiplier as the step size (1 - stepSize for "down", 1 + stepSize for "up")
+            stepSize = uniform(0.01, 0.2)
+            # get a 2-tuple of the parameters of the better of the 2 steps, up or down, and the likelihood with those parameters
+            # NB: currentGuess and currentLikelihood will be returned unchanged if neither step direction is an improvement.
+            result = updateGuessAndLikelihood(currentGuess, stepSize, currentLikelihood, direction, sList)
+            currentGuess = result[0]
+            currentLikelihood = result[1]
+            stepsLeft -= 1
+        # at the end of a run, update the best guess and its likelihood, if there is an improvement
+        if currentLikelihood > bestLikelihood:
+            bestLikelihood = currentLikelihood
+            bestGuess = currentGuess
+        restarts -= 1
+    return bestGuess
+
+def updateGuessAndLikelihood(currentGuess, stepSize, currentLikelihood, index, sList):
+    oldValue = currentGuess[index]
+    lower = oldValue * (1 - stepSize)
+    if lower < 0:
+        lower = oldValue / 2
+        
+    higher = oldValue * (1 + stepSize)
+    if higher > 1:
+        higher = (1 + oldValue) / 2.0
+
+    currentGuess[index] = lower
+    lowResult = listLikelihood(currentGuess, sList)
+    currentGuess[index] = higher
+    highResult = listLikelihood(currentGuess, sList)
+    if lowResult > currentLikelihood or highResult > currentLikelihood:
+        if lowResult > highResult:
+            currentGuess[index] = lower
+            currentLikelihood = lowResult
+        else:
+            currentGuess[index] = higher
+            currentLikelihood = highResult
+    else:
+        currentGuess[index] = oldValue
+        
+    return (currentGuess, currentLikelihood)
     
 # minimize the negative of likelihood, to maximize likelihood
 def negativeLikelihood(x, sList):
@@ -97,6 +167,16 @@ def negativeLikelihood(x, sList):
 # if the parameters are a better match for sList. uses MLE method,
 # taking the sum of the log likelihood function results instead of the
 # product of the likelihood function results.
+'''
+            uList                vList
+0           u[0]^2               u[0]^2
+1           u[1]^2               w + a u[1]^2 + b v[0]^2
+2           u[2]^2               w + a u[2]^2 + b u[1]^2
+
+to compare:
+            u[1]^2               v[0]^2
+            u[2]^2               v[1]^2
+'''
 def listLikelihood(x, sList):
     # get list of daily percent changes
     uList = getUList(sList)
@@ -106,8 +186,8 @@ def listLikelihood(x, sList):
     
     i = 0
     mySum = 0
-    while i < len(vList):
-        mySum += logLikelihoodFunction(uList[i], vList[i])
+    while i < len(uList) - 1:
+        mySum += logLikelihoodFunction(uList[i + 1], vList[i])
         i += 1
     return mySum
 
@@ -141,6 +221,64 @@ def getVList(x, uList):
         i += 1
     return result
 
+# values: a time series of values to find the autocorrelation
+# lag: the number of steps between values in the autocorrelation
+#
+# result: autocorrelation of values with offset of "lag"
+# result = E((xi - x-bar) (xi+lag - x-bar)) / sigma^2
+# where x-bar is the sample mean, sigma^2 is the variance
+def autocorr(values, lag):
+    total = 0
+    myMean = mean(values)
+    i = lag
+    while i < len(values):
+        total += (values[i] - myMean) * (values[i-lag] - myMean)
+        i += 1
+    cov = float(total) / (len(values) - lag)
+    return cov / var(values)
+
+# values: a time series of values to find the autocorrelation
+# maxLag: the maximum number of steps between values in the autocorrelation.
+#
+# result: a list of autocorrelations, with lags from 1 to maxLag.
+def autocorrSeries(values, maxLag):
+    result = []
+    i = 1
+    while i <= maxLag:
+        result.append(autocorr(values, i))
+        i += 1
+    return result
+
+# values: a time series of values to find the autocorrelation
+# maxLag: the maximum number of steps between values to analyze.
+#
+# result: the Ljung-Box statistic, a measure of autocorrelation.
+# result = m (m + 2) Sum i:1->maxLag (ni^2 / (m-i))
+# where:
+# m = number of observations in time series
+# maxLag = number of lags considered
+# ni = autocorrelation of the data points for lag of i
+def ljungBox(values, maxLag):
+    m = len(values)
+    total = 0
+    i = 1
+    while i <= maxLag:
+        corr = autocorr(values, i)
+        total += (corr ** 2) / float(m - i)
+        i += 1
+    return m * (m + 2) * total
+
+# values: a time series of values to find the autocorrelation
+# maxLag: the maximum number of steps between values to analyze.
+#
+# result: the p-value for the hypothesis that there is autocorrelation
+# within a lag of maxLag or less in the data set of values.
+# p-value = 1 - Pr(chi-squared value)
+# note: Ljung-Box statistic has a chi-squared distribution, with
+# degrees of freedom equal to the maximum lag
+def ljungBoxPValue(values, maxLag):
+    return 1 - stats.chi2.cdf(ljungBox(values, maxLag), maxLag)
+
 # omega: scalar parameter in GARCH(1,1) model
 # alpha: ui^2 coefficient in GARCH(1,1) model
 # beta: sigma-i^2 coefficient in GARCH(1,1) model 
@@ -153,7 +291,59 @@ def getVList(x, uList):
 def getVl(omega, alpha, beta):
     return omega / (1 - alpha - beta)
 
+# sList: list of stock closing prices as a daily time series
+#
+# result: no return value.
+# prints report evaluating quality of GARCH(1,1) estimates of volatility.
+# omega: scalar parameter in GARCH(1,1) model
+# alpha: ui^2 coefficient in GARCH(1,1) model
+# beta: sigma-i^2 coefficient in GARCH(1,1) model
+# sigma-i^2 = omega + alpha * ui-1^2 + beta * sigma-i-1^2
+# autocorrelation:  E((xi - x-bar) (xi+lag - x-bar)) / sigma^2
+# where x-bar is the sample mean, sigma^2 is the variance
+# lists autocorrelation by lag, from 1-15, for u(i)^2 and u(i)^2/sigma(i)^2,
+# where u(i)^2 is the square of the percent change in stock price from day to day,
+# and sigma(i)^2 is the GARCH(1,1) estimate of variance from day to day.
+# Ljung-Box is the Ljung-Box statistic with 15 degrees of freedom for 15 lags,
+# for either u(i)^2 or u(i)^2/sigma(i)^2.
+# the P-Value is obtained from the Ljung-Box statistic; if it is lower than 0.05, reject
+# the hypothesis of no autocorrelation for lags less than or equal to 15.
+def printGarchReport(sList):
+    uList = getUList(sList)
+    params = myNaiveGarchParams(sList)
+    #params = garchParams(sList)
+    likelihood = listLikelihood(params, sList)
+    vList = getVList(params, uList)
+    currentVolatility = sqrt(vList[len(vList) - 1] * 252)
+    maxLag = 15
+    i = 0
+    while i < len(uList):
+        uList[i] = uList[i] ** 2
+        if uList[i] != 0:
+            vList[i] = vList[i] / uList[i]
+        i += 1
+    uAutocorrs = autocorrSeries(uList, maxLag)
+    quotientAutocorrs = autocorrSeries(vList, maxLag)
+    i = 0
+    print "omega: " + str(params[0]) + "\talpha: " + "%.3f" % params[1] + "\tbeta: " + "%.3f" % params[2]
+    print "likelihood score: " + str(likelihood)
+    print "Current volatility: " + str(currentVolatility)
+    print ""
+    print "autocorrelation:"
+    print "lag \t\tu^2\t\tu^2/s^2"
+    while i < maxLag:
+        print str(i + 1) + "\t\t" + "%.3f" % uAutocorrs[i] + "\t\t" + "%.3f" % quotientAutocorrs[i]
+        i += 1
+    print ""
+    uLjung = ljungBox(uList, maxLag)
+    quotientLjung = ljungBox(vList, maxLag)
+    uLjnugPValue = ljungBoxPValue(uList, maxLag)
+    quotientLjnugPValue = ljungBoxPValue(vList, maxLag)
+    print "Ljung-Box\t" + "%.3f" % uLjung + "\t\t" + "%.3f" % quotientLjung
+    print "P-Value\t\t" + "%.6f" % uLjnugPValue + "\t" + "%.6f" % quotientLjnugPValue
+
 if __name__ == '__main__':
+    '''
     myDate = dateHandler.getDate(2012, 1, 1)
     stocks = stockSymbols.getDjiaSymbols()
     
@@ -187,4 +377,29 @@ if __name__ == '__main__':
     print optionAnalysis.simpleSigma(prices)
     #print garchAverage(params[0], params[1], params[2], prices, 252)
     #print garchFutureDaily(params[0], params[1], params[2], prices, 30) * 252
+    '''
+    
+    
+    myDate = dateHandler.getDate(2012, 1, 1)
+    prices = stockData.getLastYearClosingPrices("KO", myDate)
+    printGarchReport(prices)
+    
+    
+    '''
+    myDate = dateHandler.getDate(2012, 1, 1)
+    stocks = stockSymbols.getDjiaSymbols()
+    
+    for symbol in stocks:
+        prices = stockData.getLastYearClosingPrices(symbol, myDate)
+        params = garchParams(prices)
+        print symbol
+        print params
+        print sqrt(garchZeroDaily(params[0], params[1], params[2], prices) * 252)
+        print optionAnalysis.sigma(prices)
+        params = myNaiveGarchParams(prices)
+        print params
+        print sqrt(garchZeroDaily(params[0], params[1], params[2], prices) * 252)
+        print optionAnalysis.sigma(prices)
+        print ""
+    '''
     pass
