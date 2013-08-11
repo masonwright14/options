2b9436f9b28002db7c5593dcf6f7b423cd6e8f8e
masonwright <masonwright14@gmail.com>
2013-08-11 16:20:15 -0500

    added new Monte Carlo procedures with variance reduction methods. Note that the best variance reduction for the run time seems to be with the antithetic variable technique, not moment matching or other more complex techniques.

--- src/python/monteCarlo.py
+++ src/python/monteCarlo.py
@@ -39,6 +39,127 @@ def gbmMonteCarlo(x, k, r, t, sigma, steps, samples):
 # sigma = square root of variance rate of return of the underlying
 # samples = number of sample runs to take
 #
+# result = list of result values of a call option, using the stratified
+# approach to variance reduction
+def stratifiedOneStepGbmMonteCarlo(x, k, r, t, sigma, samples):
+    # S(T) = S(0) e^((r - sigma^2 / 2) T + sigma Z sqrt(T))
+    a = (r - (sigma ** 2) / 2) * t
+    b = sigma * sqrt(t)
+    result = []
+    iteration = 0
+    denom = samples + 1.0
+    while iteration < samples:
+        s = x * exp(a + b * norm.ppf((iteration + 1.0) / denom))
+        s = max(0, s - k)
+        result.append(s)
+        iteration += 1
+    return result
+
+# x = underlying price in dollars
+# k = strike price in dollars
+# r = risk free interest rate, adjusted as continuously compounding
+# t = time to maturity in years
+# sigma = square root of variance rate of return of the underlying
+# samples = number of sample runs to take
+#
+# result = list of result values of a call option, using the moment
+# matching variance reduction technique, matching first and second moments,
+# combined with the antithetic variable technique
+def antitheticMomentMatchingOneStepGbmMonteCarlo(x, k, r, t, sigma, samples):
+    # S(T) = S(0) e^((r - sigma^2 / 2) T + sigma Z sqrt(T))
+    a = (r - (sigma ** 2) / 2) * t
+    b = sigma * sqrt(t)
+    result = []
+    i = 0
+    randomVariables = []
+    while i < samples:
+        randomVariables.append(gauss(0, 1))
+        i += 1
+    myMean = mean(randomVariables)
+    myStd = std(randomVariables)
+    i = 0
+    while i < len(randomVariables):
+        randomVariables[i] = (randomVariables[i] - myMean) / myStd
+        i += 1
+    iteration = 0
+    while iteration < samples:
+        bTemp = b * randomVariables[iteration]
+        s1 = x * exp(a + bTemp)
+        s2 = x * exp(a - bTemp)
+        s1 = max(0, s1 - k)
+        s2 = max(0, s2 - k)
+        sMean = (s1 + s2) / 2.0
+        result.append(sMean)
+        iteration += 1
+    return result
+
+# x = underlying price in dollars
+# k = strike price in dollars
+# r = risk free interest rate, adjusted as continuously compounding
+# t = time to maturity in years
+# sigma = square root of variance rate of return of the underlying
+# samples = number of sample runs to take
+#
+# result = list of result values of a call option, using the moment
+# matching variance reduction technique, matching first and second moments
+def momentMatchingOneStepGbmMonteCarlo(x, k, r, t, sigma, samples):
+    # S(T) = S(0) e^((r - sigma^2 / 2) T + sigma Z sqrt(T))
+    a = (r - (sigma ** 2) / 2) * t
+    b = sigma * sqrt(t)
+    result = []
+    i = 0
+    randomVariables = []
+    while i < samples:
+        randomVariables.append(gauss(0, 1))
+        i += 1
+    myMean = mean(randomVariables)
+    myStd = std(randomVariables)
+    i = 0
+    while i < len(randomVariables):
+        randomVariables[i] = (randomVariables[i] - myMean) / myStd
+        i += 1
+    iteration = 0
+    while iteration < samples:
+        s = x * exp(a + b * randomVariables[iteration])
+        result.append(s)
+        iteration += 1
+    for i in range(0, len(result) - 1):
+        result[i] = max(0, result[i] - k) 
+    return result
+
+# x = underlying price in dollars
+# k = strike price in dollars
+# r = risk free interest rate, adjusted as continuously compounding
+# t = time to maturity in years
+# sigma = square root of variance rate of return of the underlying
+# samples = number of sample runs to take
+#
+# result = list of result values of a call option, using the antithetic
+# approach to variance reduction
+def antitheticOneStepGbmMonteCarlo(x, k, r, t, sigma, samples):
+    # S(T) = S(0) e^((r - sigma^2 / 2) T + sigma Z sqrt(T))
+    a = (r - (sigma ** 2) / 2) * t
+    b = sigma * sqrt(t)
+    result = []
+    iteration = 0
+    while iteration < samples:
+        bTemp = b * gauss(0, 1)
+        s1 = x * exp(a + bTemp)
+        s2 = x * exp(a - bTemp)
+        s1 = max(0, s1 - k)
+        s2 = max(0, s2 - k)
+        sMean = (s1 + s2) / 2.0
+        result.append(sMean)
+        iteration += 1
+    return result
+
+# x = underlying price in dollars
+# k = strike price in dollars
+# r = risk free interest rate, adjusted as continuously compounding
+# t = time to maturity in years
+# sigma = square root of variance rate of return of the underlying
+# samples = number of sample runs to take
+#
 # result = list of result values of a call option
 def oneStepGbmMonteCarlo(x, k, r, t, sigma, samples):
     # S(T) = S(0) e^((r - sigma^2 / 2) T + sigma Z sqrt(T))
@@ -154,6 +275,7 @@ def checkCallOptionValue(w, x, k, r, t, sigma, confidenceLevel):
     return ci[0] <= w and ci[1] >= w
 
 if __name__ == '__main__':
+    '''
     result = gbmMonteCarlo(42, 40, 0.1, 0.5, 0.2, 100, 1000)
     print mean(result)
     print std(result)
@@ -183,4 +305,32 @@ if __name__ == '__main__':
     print str(ci[0]) + " " + str(ci[1])
     
     print checkCallOptionValue(5.0, 42, 40, 0.1, 0.5, 0.2, 0.95)
+
+    result = stratifiedOneStepGbmMonteCarlo(42, 40, 0.1, 0.5, 0.2, 10000)
+    print mean(result)
+    print std(result)
+    print standardError(result)
+    ci = confidenceInterval(result, 0.95)
+    print str(ci[0]) + " " + str(ci[1])
+    '''
+    result = antitheticOneStepGbmMonteCarlo(42, 40, 0.1, 0.5, 0.2, 100000)
+    print mean(result)
+    print std(result)
+    print standardError(result)
+    ci = confidenceInterval(result, 0.95)
+    print str(ci[0]) + " " + str(ci[1])
+    
+    result = momentMatchingOneStepGbmMonteCarlo(42, 40, 0.1, 0.5, 0.2, 100000)
+    print mean(result)
+    print std(result)
+    print standardError(result)
+    ci = confidenceInterval(result, 0.95)
+    print str(ci[0]) + " " + str(ci[1])
+    
+    result = antitheticMomentMatchingOneStepGbmMonteCarlo(42, 40, 0.1, 0.5, 0.2, 100000)
+    print mean(result)
+    print std(result)
+    print standardError(result)
+    ci = confidenceInterval(result, 0.95)
+    print str(ci[0]) + " " + str(ci[1])
     pass
